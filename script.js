e3547f1156bd1552165ece0742de72191dec2380
Aung662001 <107274438+Aung662001@users.noreply.github.com>
2023-04-03 12:09:32 +0630

    some js added

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const listContainer = document.querySelector("[data-lists]");
 const newListForm = document.querySelector("[data-new-list-form]");
 const newListInput = document.querySelector("[data-new-list-input]");
+const deleteListButton = document.querySelector("[ data-delete-list-button]");
 
 const LIST_FORM_KEY = "list-form";
 const SELECTED_LIST_FORM_ID_KEY = "task.selectedListId";
 
-const lists = JSON.parse(localStorage.getItem(LIST_FORM_KEY)) || [];
-const selectedList = localStorage.getItem(SELECTED_LIST_FORM_ID_KEY);
+let lists = JSON.parse(localStorage.getItem(LIST_FORM_KEY)) || [];
+let selectedListId = localStorage.getItem(SELECTED_LIST_FORM_ID_KEY);
 newListForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const listInput = newListInput.value;
+  if (listInput === null || listInput === "") return;
   const list = createList(listInput);
-  console.log(listInput);
   newListInput.value = null;
   lists.push(list);
   saveAndRender();
 });
 
+listContainer.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "li") {
+    selectedListId = e.target.dataset.listId;
+    saveAndRender();
+  }
+});
+
 function save() {
   localStorage.setItem(LIST_FORM_KEY, JSON.stringify(lists));
+  localStorage.setItem(SELECTED_LIST_FORM_ID_KEY, selectedListId);
 }
 function saveAndRender() {
   save();
@@ -37,8 +46,8 @@ function render() {
     listElement.classList.add("list-name");
     listElement.innerText = list.name;
     listContainer.append(listElement);
-    if (list.id === selectedList) {
-      selectedList.classList.add("active-list");
+    if (list.id === selectedListId) {
+      listElement.classList.add("active-list");
     }
   });
 }
@@ -48,3 +57,10 @@ function clearElements(element) {
   }
 }
 render();
+
+deleteListButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  lists = lists.filter((list) => list.id != selectedListId);
+  selectedListId = null;
+  saveAndRender();
+});
